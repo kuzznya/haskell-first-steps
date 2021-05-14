561cde408703020281e6e8a046a13173872474a0
Ilya Kuznetsov <ikuz2002@gmail.com>
2021-05-14 17:15:30 +0300

    refactor: Use pattern matching in merge sort

--- src/Sort.hs
+++ src/Sort.hs
@@ -19,6 +19,8 @@ mergesort xs =
         merge [] [] = []
         merge xs [] = xs
         merge [] ys = ys
-        merge (x:xs) (y:ys) = if x <= y then x : merge xs (y:ys) else y : merge (x:xs) ys
+        merge (x:xs) (y:ys)
+            | x <= y    = x : merge xs (y:ys) 
+            | otherwise = y : merge (x:xs) ys
     in
         merge (mergesort firstHalf) (mergesort secondHalf)
